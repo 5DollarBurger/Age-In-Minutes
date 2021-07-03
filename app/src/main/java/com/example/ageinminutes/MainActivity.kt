a5f1262e7b41f87343b089cf26bb2290d35af6ac
Darrell Leong <a843183@yara.com>
2021-07-03 22:41:46 +0800

    completed age in minutes function

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import com.example.ageinminutes.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -35,13 +36,35 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(
+        val dpd = DatePickerDialog(
             this,
             DatePickerDialog.OnDateSetListener {
-                    view, year, month, dayOfMonth ->
+                    view, selectedYear, selectedMonth, selectedDayOfMonth ->
 
+                // Report input date
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                binding.tvSelectedDate.setText(selectedDate)
+
+                // Process selected date to datetime object
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+
+                // Time number of minutes from reference 1970 to selected date
+                val selectedDateInMinutes = theDate!!.time / 60000 // Assuring that not null
+
+                // Time number of minutes from reference 1970 to current date
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMinutes = currentDate!!.time / 60000
+
+                // Calculate number of minutes difference
+                val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+                binding.tvSelectedDateinMinutes.setText(differenceInMinutes.toString())
             },
             year, month, day
-        ).show()
+        )
+
+        // Only past dates allowed to be selected in calendar
+        dpd.datePicker.setMaxDate(Date().time - 86400000)
+        dpd.show()
     }
 }
